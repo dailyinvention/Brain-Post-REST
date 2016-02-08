92961d664349546d2b3bf3113bdd453a65968142
Stefan Holodnick <stefan@dailyinvention.com>
2016-02-07 22:02:55 -0500

    Added get service to retreive brain data.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var redis = require('redis');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('json spaces', 40);
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -31,7 +32,7 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log('Request received.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -41,6 +42,34 @@ router.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
+
+//Get brain stats
+
+router.route('/neurobrainget')
+    .get(function(req, res) {
+	objects= [];
+    	redis_client.keys('*', function (err, keys) {
+	    //res.json(keys);
+	    var key, _i, _len, seen;
+    	    seen = 0;
+            for (_i = 0, _len = keys.length; _i < _len; _i++) {
+		(function(i) {
+      		     redis_client.hgetall(keys[i], function(err, obj) {
+        	          objects.push({key:keys[i],obj});
+        	          seen++;
+        	          if (seen == _len) {
+          	             return res.end(JSON.stringify(objects));
+       		          }
+      	    	     })
+		})(_i)
+		
+    	    } 
+	});     
+    });
+    
+
+//Post brain stats
+
 router.route('/neurobrainpost')
 
     .post(function(req, res) {
@@ -60,9 +89,15 @@ router.route('/neurobrainpost')
       var beta = req.body.beta
       var gamma = req.body.gamma
 
-      console.log(delta)
+      console.log("attention: " + attention)
+      console.log("meditation: " + meditation)
+      console.log("delta: " + delta)
+      console.log("theta: " + theta)
+      console.log("alpha: " + alpha)
+      console.log("beta: " + beta)
+      console.log("gamma:" + gamma)
 
-      redis_client.hmset([date_string,"attention",attention,"meditation",meditation,"delta",delta,"theta",theta,"alpha",alpha,"beta",beta,"gamma",gamma],function(error, result) 	  {
+      redis_client.hmset(["input:" + date_string,"attention",attention,"meditation",meditation,"delta",delta,"theta",theta,"alpha",alpha,"beta",beta,"gamma",gamma],function(error, result) 	  {
       	if (error)
             res.send(error);
 
