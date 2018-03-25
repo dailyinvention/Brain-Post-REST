dd2e24cf3a8ce1d0c17059d3b7e71dd5901eaeeb
Stefan Holodnick <stefan@dailyinvention.com>
2018-03-24 22:16:33 -0400

    Fixed so returning values every millisecond.

--- ws-server.js
+++ ws-server.js
@@ -19,68 +19,49 @@ app.use(function (req, res) {
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
+let lastJSON = {
+  date: ''
+}
+let connections = []
+
 // On web socket connection
 wss.on('connection', function connection (ws, req) {
-  const location = url.parse(req.url, true)
+  // const location = url.parse(req.url, true)
+  let clientID = parseInt(url.parse(req.url, true).query.clientID)
+  connections[clientID] = ws
+
+  console.log(clientID)
   // You might use location.query.access_token to authenticate or share sessions
   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
   // When web socket receives message, create object and store in Redis.
   ws.on('message', function incoming (message) {
-    console.log('received:', message)
-    wss.clients.forEach(function each (client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        let messageObj = JSON.parse(message)
-        let date = new Date()
-        let day = date.getDate()
-        let month = date.getMonth() + 1
-        let year = date.getFullYear()
-        let hours = date.getHours()
-        let minutes = date.getMinutes()
-        let seconds = date.getSeconds()
-        let dateString = year + '-' + month + '-' + day + ' ' + hours + '_' + minutes + '_' + seconds
-        let attention = messageObj.attention
-        let meditation = messageObj.meditation
-        let delta = messageObj.delta
-        let theta = messageObj.theta
-        let alpha = messageObj.alpha
-        let beta = messageObj.beta
-        let gamma = messageObj.gamma
-      
-        console.log('attention: ' + attention)
-        console.log('meditation: ' + meditation)
-        console.log('delta: ' + delta)
-        console.log('theta: ' + theta)
-        console.log('alpha: ' + alpha)
-        console.log('beta: ' + beta)
-        console.log('gamma:' + gamma)
-      
-        // Store in Redis
-        redisClient.hmset(
-          [
-            dateString,
-            'attention',
-            attention,
-            'meditation',
-            meditation,
-            'delta',
-            delta,
-            'theta',
-            theta,
-            'alpha',
-            alpha,
-            'beta',
-            beta,
-            'gamma',
-            gamma
-          ],
-          (error, result) => {
-            if (error) console.log(error)
-          }
-        )
-        client.send(message)
+    let messageObj = JSON.parse(message)
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let newMessageObj = {
+      date: year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds,
+      attention: messageObj.attention,
+      meditation: messageObj.meditation,
+      delta: messageObj.delta,
+      theta: messageObj.theta,
+      alpha: messageObj.alpha,
+      beta: messageObj.beta,
+      gamma: messageObj.gamma
+    }
+
+    if (newMessageObj.date !== lastJSON.date) {
+      lastJSON = newMessageObj
+      if (connections[1]) {
+        connections[1].send(JSON.stringify(newMessageObj))
+        console.log('newMessageObj: ' + JSON.stringify(newMessageObj))
       }
-    })
+    }
   })
 })
 
